47b5d52f9e1dab7d1c1dca79fd3a07a90ae83b8b
Vilmor Somera <vsomera77@outlook.com>
2023-05-27 21:56:02 -0700

    navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import ChatDashboard from "./pages/Chat"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Navbar from "./components/Navbar"
@@ -12,7 +13,7 @@ function App() {
         <Navbar />
         <div className="container">
           <Routes>
-            {/* <Route path="/" element={ } /> */}
+            <Route path="/" element={ <ChatDashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Routes>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { FaSignInAlt, FaRegUser, FaSignOutAlt, FaRocketchat } from "react-icons/fa"
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
@@ -5,19 +6,26 @@ const Navbar = () => {
         <>
             <nav>
                 <div className="nav-content">
-                    <div>
-                        <h1>ChatMate</h1>
+                    <div id="title">
+                        <Link to="/">
+                            <h1> <FaRocketchat className="icon" /> Chat<a>Mate</a></h1>
+                        </Link>
                     </div>
 
                     <ul>
                         <li>
-                            <Link to="/login"> 
-                                Login
+                            <Link className="nav-link" to="/login">
+                                <FaSignInAlt className="icon" />  Login
                             </Link>
                         </li>
                         <li>
-                            <Link to="/register"> 
-                                Register
+                            <Link className="nav-link" to="/register">
+                                <FaRegUser className="icon" />  Register
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="nav-link" to="/login">
+                                <FaSignOutAlt className="icon" />  LogOut
                             </Link>
                         </li>
                     </ul>

59059a637611283bd7a7cf0b681283e6f3cb7903
Vilmor Somera <vsomera77@outlook.com>
2023-05-27 23:26:02 -0700

    initiate firebase i

---
+++ src/config/firebase.ts
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { 
+    getAuth, 
+    GoogleAuthProvider, 
+    GithubAuthProvider, 
+    OAuthProvider } from "firebase/auth" // user auth methods
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCytb2K-vteJk4RhXyAdj7qJb_thi2Av14",
+  authDomain: "chatmate-d177e.firebaseapp.com",
+  projectId: "chatmate-d177e",
+  storageBucket: "chatmate-d177e.appspot.com",
+  messagingSenderId: "613468805852",
+  appId: "1:613468805852:web:0d45572f51173184a8b219",
+  measurementId: "G-P3YJJB98JW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app) 
+
+export const googleProvider = new GoogleAuthProvider() 
+export const gitHubProvider = new GithubAuthProvider()
+export const microsoftProvider = new OAuthProvider('microsoft.com');

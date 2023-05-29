fcb8a61177077fb7202aa4dc747b692ce6ee2a5b
Vsomera <vsomera77@outlook.com>
2023-05-29 16:21:09 -0700

    redirect to login page if user is not logged in

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,4 +1,22 @@
+import { auth } from "../config/firebase"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
 const ChatPage = () => {
+
+    const navigate = useNavigate()
+    
+    useEffect(() => {
+       auth.onAuthStateChanged((user) => {
+        if (user == null) {
+            navigate("/login")
+        }
+       })
+
+        console.log(auth.currentUser)
+
+    }, [navigate])
+
     return (
         <div className="chat-container">
             Chat Dashboard

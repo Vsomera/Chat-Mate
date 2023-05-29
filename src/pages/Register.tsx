50cfec2967e07e04de2c9e912deacaaac7c180e4
Vsomera <vsomera77@outlook.com>
2023-05-29 16:19:57 -0700

    authenticate and register users into firebase

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,13 +1,35 @@
 import { FaRegUser } from "react-icons/fa"
-import { useState } from "react"
-import tst from "../images/trees.svg"
-
+import { toast } from 'react-toastify';
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react"
+import { auth, googleProvider } from "../config/firebase"
+import { createUserWithEmailAndPassword } from "firebase/auth"
 
 const Register = () => {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
+
+    const navigate = useNavigate()
+
+    const registerEmailPassword = async () => {
+        try {
+            // registers a user into firebase with email and password
+            await createUserWithEmailAndPassword(auth, email, password)
+            
+            toast.success(`Welcome ${name}`)
+            navigate("/")
+            
+        } catch (err) { // TODO fix
+            const errorMessage = (err as Error).message
+            toast.error(`${errorMessage}`)
+        }
+        console.log(auth.currentUser)
+        // empties text box
+        setEmail("")
+        setPassword("")
+    }
 
     return (
         <>
@@ -28,8 +50,9 @@ const Register = () => {
                             <h1><FaRegUser className="icon" /> Register</h1>
                         </div>
 
-                        <form className="register-group" onSubmit={undefined}>
+                        <form className="register-group">
                             <input
+                                id="name"
                                 className="input"
                                 type="name"
                                 value={name}
@@ -37,6 +60,7 @@ const Register = () => {
                                 placeholder="Please enter a Username" />
                             <div className="underline"></div>
                             <input
+                                id="email"
                                 className="input"
                                 type="email"
                                 value={email}
@@ -44,6 +68,7 @@ const Register = () => {
                                 placeholder="Please enter your Email" />
                             <div className="underline"></div>
                             <input
+                                id="password"
                                 className="input"
                                 type="password"
                                 value={password}
@@ -52,6 +77,7 @@ const Register = () => {
                             <div className="underline"></div>
 
                             <input
+                                id="password"
                                 className="input"
                                 type="password"
                                 value={confirmPassword}
@@ -59,7 +85,7 @@ const Register = () => {
                                 placeholder="Confirm Password" />
                             <div className="underline"></div>
                         </form>
-                        <button >Sign In</button>
+                        <button onClick={registerEmailPassword}>Sign In</button>
                     </section>
 
                 </div>

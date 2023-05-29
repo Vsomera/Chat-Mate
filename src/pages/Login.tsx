60774b465e03f94b079303edf33e79d714c56e53
Vilmor Somera <vsomera77@outlook.com>
2023-05-28 21:25:15 -0700

    inherit backround from auth-background div

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,18 @@
 const Login = () => {
     return (
         <>
-            <div className="auth-container">
-                Login
+            <div className="auth-background">
+                <div className="auth-container">
+
+                    <section className="login-content">
+                        Login Content
+                    </section>
+
+                    <section className="login-form">
+                        Login Form
+                    </section>
+
+                </div>
             </div>
         </>
     )

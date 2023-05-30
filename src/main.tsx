adab6ce47130236895649698d7607741768e9e93
Vsomera <vsomera77@outlook.com>
2023-05-29 23:06:55 -0700

    better styles for react-toast messages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <ToastContainer theme="dark" position="top-center"/>
+    <ToastContainer theme="dark" position="top-center" className="toastContainerCss"/>
   </React.StrictMode>,
 )

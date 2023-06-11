506e72e02a4ba814410389b715f2c76c3515fb90
Vilmor Somera <vsomera77@outlook.com>
2023-06-11 15:08:52 -0700

    authMethods component for signing in

---
+++ src/components/AuthMethods.tsx
@@ -0,0 +1,28 @@
+import { FcGoogle } from "react-icons/fc"
+import { VscGithub } from "react-icons/vsc"
+import { CgMicrosoft } from "react-icons/cg"
+import { signInGoogle } from "../auth/authService"
+
+const AuthMethods = () => {
+    return (
+        <div className="auth-methods">
+            <div className="icon-wrapper" onClick={signInGoogle}>
+                <div className="icon-container-google">
+                    <FcGoogle className="auth-icon" />
+                </div>
+            </div>
+            <div className="icon-wrapper">
+                <div className="icon-container-github">
+                    <VscGithub className="auth-icon" />
+                </div>
+            </div>
+            <div className="icon-wrapper">
+                <div className="icon-container-microsoft">
+                    <CgMicrosoft className="auth-icon" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AuthMethods

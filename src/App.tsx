40fd696eee6d5732c9b9dc25a28047d352c398c0
Vilmor Somera <vsomera77@outlook.com>
2023-06-13 16:19:49 -0700

    implemented context api for holding user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { useState } from "react"
+import { auth } from "./config/firebase";
+import { User } from "firebase/auth";
+import { UserContext } from "./context/userContext";
 import 'react-toastify/dist/ReactToastify.css';
 import ChatPage from "./pages/ChatPage"
 import Login from "./pages/Login"
@@ -8,18 +12,29 @@ import './App.css'
 
 function App() {
 
+  const [user, setUser] = useState<User | null>(null)
+  auth.onAuthStateChanged((user) => {
+    if (user != null) {
+      setUser(user)
+    } else {
+      setUser(null)
+    }
+  })
+
   return (
     <>
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={ <ChatPage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-      </Router>
+      <UserContext.Provider value={{ user }}>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<ChatPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+        </Router>
+      </UserContext.Provider>
     </>
   )
 }

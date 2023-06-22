c9a9ee3253285bb9a3452a2d1bc08da26e004fc6
Vilmor Somera <vsomera77@outlook.com>
2023-06-22 16:54:20 -0700

    add chat css to index.css file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import "./index.css"
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { UserContextProvider } from "./context/userContext.tsx"

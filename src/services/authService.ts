5b2098bfa4160bff5588cecb5359521693f91496
Vsomera <vsomera77@outlook.com>
2023-06-22 13:12:11 -0700

    upload user.photoURL to firebase storage

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,7 +1,10 @@
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
-import { auth, gitHubProvider, googleProvider, microsoftProvider } from "../config/firebase"
+import { db, storage, auth, gitHubProvider, googleProvider, microsoftProvider } from "../config/firebase"
 import { toast } from "react-toastify"
 import { signInWithPopup } from "firebase/auth"
+import { UserCredential } from "firebase/auth"
+import { doc, setDoc } from "firebase/firestore"
+import { ref, uploadBytesResumable } from "firebase/storage"
 
 export const registerEmailPassword = async (
     username: string,
@@ -16,7 +19,7 @@ export const registerEmailPassword = async (
         await updateProfile(newUser.user, {
             displayName: username
         })
-
+        await addUsertoDb(newUser) // adds user to firestore database
         toast.success(`Welcome ${username}`)
 
     } catch (err) {
@@ -30,6 +33,7 @@ export const signInGoogle = async () => {
     try {
         // logs in a user using google account
         const user = await signInWithPopup(auth, googleProvider)
+        await addUsertoDb(user)
         toast.success(`Welcome ${user.user.displayName}`)
     } catch (err: unknown) {
         if (err instanceof Error) {
@@ -45,6 +49,7 @@ export const signInGithub = async () => {
     try {
         // logs in a user using gitHub account
         const user = await signInWithPopup(auth, gitHubProvider)
+        await addUsertoDb(user)
         toast.success(`Welcome ${user.user.displayName}`)
     } catch (err: unknown) {
         if (err instanceof Error) {
@@ -58,6 +63,7 @@ export const signInMS = async () => {
     try {
         // logs in a user using Microsoft account
         const user = await signInWithPopup(auth, microsoftProvider)
+        await addUsertoDb(user)
         toast.success(`Welcome ${user.user.displayName}`)
 
     } catch (err: unknown) {
@@ -66,5 +72,74 @@ export const signInMS = async () => {
         } else {
             toast.error('Error : Could not log in with Microsoft')
         }
+    }
+}
+
+const addUsertoDb = async (userInfo: UserCredential) => {
+    try {
+        // adds a new user to firestore user database
+        const user = userInfo.user
+
+        await uploadUserProfileImage(user.photoURL, user.uid) // adds user pfp to storage
+
+        await setDoc(doc(db, "users", user.uid), {
+            // adds a document with the user data
+            uid: user.uid,
+            displayName: user.displayName,
+            email: user.email,
+            photoURL: user.photoURL ? user.photoURL : null // checks if a user has a profile picture
+        })
+
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            toast.error(err.message)
+        } else {
+            toast.error(`Error : Could not add user data to database`)
+        }
+    }
+}
+
+const uploadUserProfileImage = async (photoURL: string | null, uid : string | null) => {
+    try {
+
+        if (photoURL) {
+            // if user has a profile picture, upload it to firebase storage\
+
+            const toDataURL = (photoURL: string) => fetch(photoURL)
+                // converts the photoURL into dataURL
+                .then(res => res.blob())
+                .then(blob => new Promise<string>((resolve, reject) => {
+                    const reader = new FileReader()
+                    reader.onloadend = () => resolve(reader.result as string)
+                    reader.onerror = reject
+                    reader.readAsDataURL(blob)
+                }))
+
+            const dataURLtoFile = (toDataURL : string, user : string) => {
+                // converts dataURL to a file
+                const arr = toDataURL.split(",")
+                const matchResult = arr[0].match(/:(.*?);/)
+                const mime = matchResult ? matchResult[1] : ''
+                const bstr = atob(arr[1])
+                let n = bstr.length
+                const u8arr = new Uint8Array(n)
+                while(n--){
+                    u8arr[n] = bstr.charCodeAt(n)
+                }
+                return new File([u8arr], user, {type:mime})
+                }
+
+
+            if (uid) {
+                // uploads the converted file as a jpg to firebase storage
+                const fileDataUrl = await toDataURL(photoURL)
+                const file = dataURLtoFile(fileDataUrl, `${uid}.jpg`)
+                const storageRef = ref(storage, uid)
+                await uploadBytesResumable(storageRef, file)
+            }
+        }
+
+    } catch (error) {
+        console.error("Error uploading user profile image:", error);
     }
 }

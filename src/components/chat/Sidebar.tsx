42f769c2aa1ccb6ae03dd5057bbe2813623a3526
Vilmor Somera <vsomera77@outlook.com>
2023-06-24 21:11:29 -0700

    moved search component to SideTopbar component

--- src/components/chat/Sidebar.tsx
+++ src/components/chat/Sidebar.tsx
@@ -1,11 +1,9 @@
-import Search from "./Search"
 import SideTopbar from "./SideTopbar"
 
 const Sidebar = () => {
     return (
         <aside>
             <SideTopbar />
-            <Search />
         </aside>
     )
 }

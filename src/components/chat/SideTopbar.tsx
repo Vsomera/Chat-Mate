2df097877f78b46b5b65b33e4467d1ac82b9aadc
Vilmor Somera <vsomera77@outlook.com>
2023-06-24 21:12:10 -0700

    styled search component

--- src/components/chat/SideTopbar.tsx
+++ src/components/chat/SideTopbar.tsx
@@ -1,7 +1,28 @@
+import { useContext, useState } from "react"
+import { UserContext } from "../../context/userContext"
+import { HiOutlineSearch } from "react-icons/hi"
+import Search from "./Search"
+
+
 const SideTopbar = () => {
+
+    const { user } = useContext(UserContext)
+    const [toggleSearch, setToggleSearch] = useState(false)
+
     return (
         <div className="side-topbar">
-            Side Topbar
+            <div className="side-content">
+                <h2>
+                    <img src={user?.photoURL || ""} className="profile-img" /> Chats
+                </h2>
+
+                <h3 onClick={
+                    () => toggleSearch 
+                        ? setToggleSearch(false) 
+                        : setToggleSearch(true)}>
+                            <HiOutlineSearch className="search-icon" /></h3>
+            </div>
+            { toggleSearch && <Search /> }
         </div>
     )
 }

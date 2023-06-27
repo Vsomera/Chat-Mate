63cf4063adbf6d387b564d7c9b84641c14b43482
Vilmor Somera <vsomera77@outlook.com>
2023-06-26 18:03:26 -0700

    show users on search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,27 +1,80 @@
-import { useState } from "react"
-import { collection, doc, setDoc } from "firebase/firestore"; 
-
+import { useContext, useState } from "react"
+import { collection, getDocs, query, where } from "firebase/firestore"
+import { UserContext } from "../context/userContext"
+import { db } from "../config/firebase"
+import { User } from "firebase/auth"
+import ReactLoading from 'react-loading';
 
 const Search = () => {
 
-    // create a state with all the user's
-    const [searchedUser, setSearchedUser] = useState("")
+    const { user } = useContext(UserContext)
+
+    const [searchedUsername, setSearchedUsername] = useState("")
+    const [searchedUsers, setSearchedUsers] = useState<User[]>([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleEnter = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            // on enter key down -> search for users
+            setIsLoading(true)
+            await searchUsers()
+            setIsLoading(false)
+        }
+    }
+
+    const searchUsers = async () => {
+        const q = query(collection(db, "users"),
+            // fetches users where the displayName is like the searchedUsername state
+            where("displayName", ">=", searchedUsername),
+            where("displayName", "<=", searchedUsername + "uf8ff"))
+
+        if (searchedUsername.length > 1) {
+            const qSnapshot = await getDocs(q)
+            const users: User[] = []
+
+            qSnapshot.forEach((doc) => {
+                // loops through users collection and pushes user data to users array
+                users.push(doc.data() as User)
+            })
+
+            setSearchedUsers(users)
+            setSearchedUsername("")
+        }
+    }
 
     return (
         <div className="search">
-            
-            <input 
-                type="text" 
+
+            <input
+                type="text"
                 className="auth-input"
-                value={searchedUser}
-                onChange={e => setSearchedUser(e.target.value)}
-                placeholder="Search for User..."/>
+                value={searchedUsername}
+                onChange={e => setSearchedUsername(e.target.value)}
+                onKeyDown={handleEnter}
+                placeholder="Search for User..." />
             <div className="underline"></div>
 
-            <div className="searched-user">
-                <img 
-                    src=""
-                    alt="" />
+            <div className="searched-users">
+
+                {searchedUsers
+                    .filter((otherUser: User) => otherUser.uid != user?.uid) // filters out the signed in user on search
+                    .map((otherUser: User) => {
+                        return (
+                            <div className="searched-user" key={otherUser.uid}>
+                                <div className="searched-container">
+                                    <img
+                                        src={otherUser.photoURL || ""}
+                                        className="profile-img" />
+                                    <div className="searched-user-info">
+                                        <p>{otherUser.displayName}</p>
+                                        <p>{otherUser.email}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
             </div>
         </div>
     )

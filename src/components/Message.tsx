0f5f2a1c7c8dbdec2c45ac14d808458ab4da9316
Vilmor Somera <vsomera77@outlook.com>
2023-07-26 17:50:15 -0700

    re-coded for fast fetching of user pfp

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,6 +1,4 @@
-import { ref, getDownloadURL } from "firebase/storage"
-import { storage } from "../config/firebase"
-import { useContext, useEffect, useState } from "react"
+import { useContext } from "react"
 import { Timestamp } from "firebase/firestore"
 import { UserContext } from "../context/userContext"
 import { ChatContext } from "../context/chatContext"
@@ -18,8 +16,7 @@ const Message = (props: Props) => {
 
     const { id, date, senderId, text } = props.message
     const { user } = useContext(UserContext)
-    const [pfpUrl, setPfpUrl] = useState("")
-    const { selectedChat, isLoading } = useContext(ChatContext)
+    const { selectedChat, isLoading, chatUsers } = useContext(ChatContext)
 
     // convert date into human readable format
     const formattedDate = date && date.toDate().toLocaleString(undefined, {
@@ -28,38 +25,26 @@ const Message = (props: Props) => {
         hour12: true,
     });
 
-
-    useEffect(() => {
-        // gets the profile picture of a user from firebase storage
-        const storageRef = ref(storage, senderId)
-
-        // get the Url of the image
-        getDownloadURL(storageRef)
-            .then((url) => {
-                setPfpUrl(url)
-            })
-    })
-
     return (
         <>
             {selectedChat && !isLoading &&
                 <div className={`message ${senderId === user?.uid && "me"}`} key={id}>
 
-                    { user?.uid !== senderId &&
+                    {user?.uid !== senderId &&
                         <img
                             className="msg-pfp"
-                            src={pfpUrl}
+                            src={chatUsers.filter((chatUser) => chatUser.uid === senderId 
+                                && chatUser.photoURL)[0]?.photoURL || ""}
                             referrerPolicy="no-referrer"
                             alt="" />
                     }
-
+                    
                     <div className={`msg-content ${user?.uid == senderId && "me"}`}>
                         <div className="msg-text">
                             <p>{text}</p>
                         </div>
                         <p>{formattedDate}</p>
                     </div>
-
                 </div>
             }
         </>
